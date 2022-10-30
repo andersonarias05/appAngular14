f67298f3153d3fdf7fe6420ff8d49915dc1a5a14
andersonarias05 <34636088+andersonarias05@users.noreply.github.com>
2022-10-30 13:50:43 -0400

    [30-10-2022 task#1] Arrow functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cursoAngular';
+
+  constructor() {
+    console.log('hi');
+    
+    const auxMap = [1,2,3,4,5].map(num => num * 2)
+    console.log("auxMap", auxMap);
+    
+    const auxForEach = [1,2,3,4,5].forEach(num => num * 2)
+    console.log("auxForEach", auxForEach);
+    
+    const auxFind = [1,2,3,4,5].find(num => num === 3)
+    console.log("auxFind", auxFind);
+    
+    const auxFindIndex = [1,2,3,4,5].findIndex(num => num === 5)
+    console.log("auxFindIndex", auxFindIndex);
+    
+    const auxIndexOf = [1,2,3,4,5].indexOf(5)
+    console.log("auxIndexOf", auxIndexOf);
+    
+    const auxIndexOfString = "Hola Mundo Test".indexOf("Test")
+    console.log("auxIndexOfString", auxIndexOfString);
+    
+    const auxFilter = [1,2,3,4,5].filter(num => num % 2 === 0)
+    console.log("auxFilter", auxFilter);
+    
+    const auxJoin = [1,2,3,4,5].join('-')
+    console.log("auxJoin", auxJoin);
+    
+    const auxSplit = "hola, mundo, test".split(',')
+    console.log("auxSplit", auxSplit);
+    
+    /*
+    private getName(name: string): string {
+      return 'hi' + name;
+    }
+    
+    /* ---Arrow function
+    const getName = (name: string) => {return "hi"+name;}
+    const getName = (name: string) => "hi" + name
+    const getName = () => "hi"
+    const getName = (name: string, lastName:string) => lastName
+    const mult = num => num *2 */
+  }
+
 }
